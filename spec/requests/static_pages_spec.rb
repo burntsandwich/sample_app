455ca2e2fbefb80151dfdb1af93ba2af8faf21f5
Miles Rowland <miles.rowland@gmail.com>
2013-06-03 21:23:26 +0100

    User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,20 +3,15 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
   	end
 
-  	it "should have the title 'Home'" do
-  		visit '/static_pages/home'
-     	page.should have_selector('title', 
-     								:text => "| Home")
-    end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About')
     end
   end
